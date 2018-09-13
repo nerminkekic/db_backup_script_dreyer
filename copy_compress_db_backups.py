b4b42d6bc3ff98ad0748f0469ba432a028fa87f5
Nermin Kekic <nermink@gmail.com>
2018-09-13 13:19:11 -0500

    Add compression feature

--- copy_compress_db_backups.py
+++ copy_compress_db_backups.py
@@ -5,8 +5,21 @@
 
 import zipfile
 import os
+import glob
 os.chdir('C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source')
+
+# Identify lattest db backup file for compression
+files = glob.iglob(
+    'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source\\*')
+latest_file = max(files, key=os.path.getctime)
+print(latest_file)
+dbBackupFile = os.path.split(latest_file)
+print(dbBackupFile[1])
 
 # Create zip file
 with zipfile.ZipFile('dbBackup.zip', 'w', allowZip64=True) as dbZip:
-    dbZip.write('file.exe')
+    dbZip.write(dbBackupFile[1])
+
+# Copy compressed file to replication folder
+
+# Delete db backups older then 3 months
