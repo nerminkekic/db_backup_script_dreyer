35850c2178c287ae937c703e7479e20c6e36746e
Nermin Kekic <nermink@gmail.com>
2018-09-15 08:18:28 -0500

    Add feature to remove files

--- copy_compress_db_backups.py
+++ copy_compress_db_backups.py
@@ -1,23 +1,30 @@
-# This script will perform three steps.
+# This script will perform four steps.
 # 1. Compress PACSIW DB Backup
 # 2. Copy PACSIW DB Backup to replication folder
-# 3. Remove PACS DB Backups that are older then 3 months to preserve space on local system
+# 3. Remove PACSIW DB Backups that are older then 3 months in source folder
+# 4. Remove PACSIW DB Backups that are older then 3 months in destination folder
 
 import zipfile
 import os
 import glob
 import shutil
+import time
+import datetime
+
+currentDate = datetime.datetime.now()
 
 # Variables
-dbBackup = 'dbBackup.zip'
+dbBackup = 'dbBackup' + str(currentDate.month) + \
+    str(currentDate.day) + str(currentDate.year) + '.zip'
 
 # Set up source and destination directories
-os.chdir(
-    'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source')
-sourceDir = 'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source\\' + dbBackup
+sourceDir = 'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source\\'
+zipFile = 'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source\\' + dbBackup
 destinationDir = 'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\destination\\'
 
 # Identify lattest db backup file for compression
+os.chdir(
+    'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source')
 files = glob.iglob(
     'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source\\*')
 latest_file = max(files, key=os.path.getctime)
@@ -29,7 +36,19 @@ with zipfile.ZipFile(dbBackup, 'w', allowZip64=True) as dbZip:
     dbZip.write(dbFile)
 
 # Copy compressed file to replication folder
-shutil.copy2(sourceDir, destinationDir)
+shutil.copy2(zipFile, destinationDir)
 
+# Delete db backups older then 30 days
+files = glob.iglob(
+    'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\source\\*')
+now = time.time()
+for f in files:
+    if(os.stat(f).st_mtime < now - 30 * 86400):
+        os.remove(f)
 
-# Delete db backups older then 3 months
+# Delete db backups older then 30 days in destination folder
+destinationFiles = glob.iglob(
+    'C:\\Users\\Nermin\\Desktop\\Projects\\PACSIW Script\\destination\\*')
+for f in destinationFiles:
+    if(os.stat(f).st_mtime < now - 30 * 86400):
+        os.remove(f)
